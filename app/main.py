a589add570970cae2f65ed897aea43e49a62614b
mini <ko_mini@naver.com>
2025-07-27 02:17:53 +0900

    feat: 동적 포트 할당 -> 고정 포트

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.api import (
-    connections,
+    connect_driver,  # 드라이버 확인
     health,  # 헬스 체크
 )
 from app.api.api_router import api_router
 from app.core.exceptions import APIException, api_exception_handler, generic_exception_handler
-from app.core.port import get_available_port  # 동적 포트 할당
 
 app = FastAPI()
 
@@ -17,21 +16,12 @@ app = FastAPI()
 app.add_exception_handler(APIException, api_exception_handler)
 app.add_exception_handler(Exception, generic_exception_handler)
 
-# 드라이버 확인 라우터
-app.include_router(connections.router)
-
 # 라우터
 app.include_router(health.router)
 app.include_router(api_router, prefix="/api")
-
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, FastAPI Backend!"}
+app.include_router(connect_driver.router, prefix="/api")
 
 
 if __name__ == "__main__":
-    # 동적 할당 로직
-    port = get_available_port()
     # Uvicorn 서버를 시작합니다.
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=39722)
