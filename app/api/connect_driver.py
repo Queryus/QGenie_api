e3fbb4b29f617984e651c98b6b19d7d4c645f4f9
mini <ko_mini@naver.com>
2025-08-02 21:32:58 +0900

    refactor: 공통 응답으로 수정

--- app/api/connect_driver.py
+++ app/api/connect_driver.py
@@ -2,18 +2,22 @@
 
 from fastapi import APIRouter
 
-from app.schemas.driver_info import DriverEnum, DriverInfoResponse
+from app.assets.driver_enum import DriverEnum
+from app.core.exceptions import APIException
+from app.core.status import CommonCode
+from app.schemas.driver_info import DriverInfo
+from app.schemas.response import ResponseMessage
 from app.services.driver_info_provider import db_driver_info
 
-router = APIRouter(tags=["Driver"])
+router = APIRouter()
 
 
-@router.get(
-    "/connections/drivers/{driver_id}",
-    summary="DB 드라이버 정보 조회 API",
-    response_model=DriverInfoResponse,
-)
-def read_driver_info(driver_id: DriverEnum):
-    module = driver_id.driver_module
-    db_type = driver_id.value
-    return db_driver_info(db_type, module)
+@router.get("/drivers/{driverId}", response_model=ResponseMessage[DriverInfo], summary="DB 드라이버 정보 조회 API")
+def read_driver_info(driverId: str):
+    """DB 드라이버 정보 조회"""
+    for driver in DriverEnum:
+        if driver.db_type == driverId:
+            return ResponseMessage.success(value=db_driver_info(driver.db_type, driver.driver_module))
+
+    # db_type 초기 유효성 검사 실패시
+    raise APIException(CommonCode.INVALID_ENUM_VALUE)
