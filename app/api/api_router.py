c63189a107013f6d612191a47fa5f97b237e7b92
mini <ko_mini@naver.com>
2025-08-02 21:31:04 +0900

    refactor: connect_driver router -> api_router로 이동

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api import test_api
+from app.api import connect_driver, test_api
+
 api_router = APIRouter()
 
 # 테스트 라우터
 api_router.include_router(test_api.router, prefix="/test", tags=["Test"])
 
 # 라우터
-# api_router.include_router(connect_driver.router, prefix="/connections", tags=["Driver"])
+api_router.include_router(connect_driver.router, prefix="/connections", tags=["Driver"])
--- app/main.py
+++ app/main.py
@@ -3,10 +3,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api import (
-    connect_driver,  # 드라이버 확인
-    health,  # 헬스 체크
-)
+from app.api import health  # 헬스 체크
 from app.api.api_router import api_router
 from app.core.exceptions import APIException, api_exception_handler, generic_exception_handler
 
@@ -19,7 +16,6 @@ app.add_exception_handler(Exception, generic_exception_handler)
 # 라우터
 app.include_router(health.router)
 app.include_router(api_router, prefix="/api")
-app.include_router(connect_driver.router, prefix="/api")
 
 
 if __name__ == "__main__":
