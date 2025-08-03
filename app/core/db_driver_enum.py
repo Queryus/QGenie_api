21bf04f0395c14004fc760f7e1bc0337a8e59606
mini <ko_mini@naver.com>
2025-08-03 17:52:29 +0900

    refactor: DB 드라이버 Enum 값 단일화

---
+++ app/core/db_driver_enum.py
@@ -0,0 +1,13 @@
+# app/core/db_driver_enum.py
+from enum import Enum
+
+
+class DBTypesEnum(Enum):
+    """지원되는 데이터베이스 드라이버 타입"""
+
+    postgresql = "psycopg2"
+    mysql = "mysql.connector"
+    sqlite = "sqlite3"
+    oracle = "cx_Oracle"
+    sqlserver = "pyodbc"
+    mariadb = "pymysql"
