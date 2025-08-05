89373976fb8d08dcc694a1fdae9639f1334f0cf0
나형진 <skgudwls@konkuk.ac.kr>
2025-08-05 14:19:58 +0900

    feat: API Key 전체 & 단일 조회

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -12,4 +12,4 @@ api_router.include_router(test_api.router, prefix="/test", tags=["Test"])
 # 라우터
 api_router.include_router(driver_api.router, prefix="/driver", tags=["Driver"])
 api_router.include_router(user_db_api.router, prefix="/user/db", tags=["UserDb"])
-api_router.include_router(api_key_api.router, prefix="/credentials", tags=["Credentials"])
+api_router.include_router(api_key_api.router, prefix="/keys", tags=["API Key"])
