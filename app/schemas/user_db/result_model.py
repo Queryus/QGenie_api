b23b8c1f48e8d8a8cdf8bf26fc0cdaaa6e68e054
ChoiseU <csw980928@gmail.com>
2025-08-09 00:15:18 +0900

    refactor: result를 공통으로 사용 하도록 수정

---
+++ app/schemas/user_db/result_model.py
@@ -0,0 +1,15 @@
+# app/schemas/user_db/result_model.py
+
+from pydantic import BaseModel, Field
+
+from app.core.status import CommonCode
+
+# 기본 반환 모델
+class BasicResult(BaseModel):
+    is_successful: bool = Field(..., description="성공 여부")
+    code: CommonCode = Field(None, description="결과 코드")
+
+# 디비 정보 저장
+class SaveProfileResult(BasicResult):
+    """DB 조회 결과를 위한 확장 모델"""
+    name: str = Field(..., description="저장된 디비명")
