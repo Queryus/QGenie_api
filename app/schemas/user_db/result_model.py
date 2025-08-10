fdb01606279bd94d1196e1bb58610e432f24a254
ChoiseU <csw980928@gmail.com>
2025-08-10 18:51:57 +0900

    refactor: save 및 update 시 사용되는 모델 공통으로 사용하도록 수정

--- app/schemas/user_db/result_model.py
+++ app/schemas/user_db/result_model.py
@@ -12,7 +12,7 @@ class BasicResult(BaseModel):
     code: CommonCode = Field(None, description="결과 코드")
 
 # 디비 정보 후 반환되는 저장 모델
-class UpdateOrSaveProfileResult(BasicResult):
+class ChangeProfileResult(BasicResult):
     """DB 조회 결과를 위한 확장 모델"""
     view_name: str = Field(..., description="저장된 디비명")
 
