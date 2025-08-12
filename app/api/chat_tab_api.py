a1b5d8df2ca7e5abf95307e2ed810166df4425f4
mini <ko_mini@naver.com>
2025-08-13 01:22:53 +0900

    feat: chat_tab_message api 추가

--- app/api/chat_tab_api.py
+++ app/api/chat_tab_api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, Path
 from app.core.response import ResponseMessage
 from app.core.status import CommonCode
 from app.schemas.chat_tab.create_model import ChatTabCreate
-from app.schemas.chat_tab.response_model import ChatTabResponse
+from app.schemas.chat_tab.response_model import ChatMessagesResponse, ChatTabResponse
 from app.schemas.chat_tab.update_model import ChatTabUpdate
 from app.services.chat_tab_service import ChatTabService, chat_tab_service
 
@@ -13,9 +13,9 @@ router = APIRouter()
 
 
 @router.post(
-    "/actions",
+    "/create",
     response_model=ResponseMessage[ChatTabResponse],
-    summary="Chat Tab 생성",
+    summary="새로운 Chat Tab 생성",
     description="새로운 Chat Tab을 생성하여 로컬 데이터베이스에 저장합니다.",
 )
 def store_chat_tab(
@@ -34,50 +34,9 @@ def store_chat_tab(
     )
     return ResponseMessage.success(value=response_data, code=CommonCode.SUCCESS_CHAT_TAB_CREATE)
 
-@router.put(
-    "/modify/{tabId}",
-    response_model=ResponseMessage[ChatTabResponse],
-    summary="특정 Chat Tab Name 수정",
-)
-def updated_chat_tab(
-    chatName: ChatTabUpdate,
-    tabId: str = Path(..., description="수정할 채팅 탭의 고유 ID"),
-    service: ChatTabService = chat_tab_service_dependency
-) -> ResponseMessage[ChatTabResponse]:
-    """
-    채팅 탭 ID를 기준으로 채팅 탭의 이름을 새로운 값으로 수정합니다.
-    - **id**: 수정할 채팅 탭 ID
-    - **name**: 새로운 채팅 탭의 이름
-    """
-    updated_chat_tab = service.updated_chat_tab(tabId, chatName)
-
-    response_data = ChatTabResponse(
-        id=updated_chat_tab.id,
-        name=updated_chat_tab.name,
-        created_at=updated_chat_tab.created_at,
-        updated_at=updated_chat_tab.updated_at,
-    )
-
-    return ResponseMessage.success(value=response_data, code=CommonCode.SUCCESS_CHAT_TAB_UPDATE)
-
-@router.delete(
-    "/remove/{tabId}",
-    response_model=ResponseMessage,
-    summary="특정 Chat Tab 삭제",
-)
-def delete_chat_tab(
-    tabId: str = Path(..., description="수정할 채팅 탭의 고유 ID"), 
-    service: ChatTabService = chat_tab_service_dependency
-) -> ResponseMessage:
-    """
-    채팅 탭 ID를 기준으로 채팅 탭을 삭제합니다.
-    - **id**: 삭제할 채팅 탭 ID
-    """
-    service.delete_chat_tab(tabId)
-    return ResponseMessage.success(code=CommonCode.SUCCESS_CHAT_TAB_DELETE)
 
 @router.get(
-    "/result",
+    "/find",
     response_model=ResponseMessage[list[ChatTabResponse]],
     summary="저장된 모든 Chat_tab 정보 조회",
     description="""
@@ -100,3 +59,71 @@ def get_all_chat_tab(
         for chat_tab in chat_tabs_in_db
     ]
     return ResponseMessage.success(value=response_data, code=CommonCode.SUCCESS_GET_CHAT_TAB)
+
+
+@router.put(
+    "/modify/{tabId}",
+    response_model=ResponseMessage[ChatTabResponse],
+    summary="특정 Chat Tab Name 수정",
+)
+def updated_chat_tab(
+    chatName: ChatTabUpdate,
+    tabId: str = Path(..., description="수정할 채팅 탭의 고유 ID"),
+    service: ChatTabService = chat_tab_service_dependency,
+) -> ResponseMessage[ChatTabResponse]:
+    """
+    채팅 탭 ID를 기준으로 채팅 탭의 이름을 새로운 값으로 수정합니다.
+    - **id**: 수정할 채팅 탭 ID
+    - **name**: 새로운 채팅 탭의 이름
+    """
+    updated_chat_tab = service.updated_chat_tab(tabId, chatName)
+
+    response_data = ChatTabResponse(
+        id=updated_chat_tab.id,
+        name=updated_chat_tab.name,
+        created_at=updated_chat_tab.created_at,
+        updated_at=updated_chat_tab.updated_at,
+    )
+
+    return ResponseMessage.success(value=response_data, code=CommonCode.SUCCESS_CHAT_TAB_UPDATE)
+
+
+@router.delete(
+    "/remove/{tabId}",
+    response_model=ResponseMessage,
+    summary="특정 Chat Tab 삭제",
+)
+def delete_chat_tab(
+    tabId: str = Path(..., description="수정할 채팅 탭의 고유 ID"),
+    service: ChatTabService = chat_tab_service_dependency,
+) -> ResponseMessage:
+    """
+    채팅 탭 ID를 기준으로 채팅 탭을 삭제합니다.
+    - **id**: 삭제할 채팅 탭 ID
+    """
+    service.delete_chat_tab(tabId)
+    return ResponseMessage.success(code=CommonCode.SUCCESS_CHAT_TAB_DELETE)
+
+
+@router.get(
+    "/find/{tabId}/messages",
+    response_model=ResponseMessage[ChatMessagesResponse],
+    summary="특정 탭의 메시지 전체 조회",
+)
+def get_chat_messages_by_tabId(
+    tabId: str = Path(..., description="채팅 탭 고유 ID"), service: ChatTabService = chat_tab_service_dependency
+) -> ResponseMessage[list[ChatMessagesResponse]]:
+    """tabId를 기준으로 해당 chat_tab의 전체 메시지를 가져옵니다."""
+    chat_tab = service.get_chat_tab_by_tabId(tabId)
+
+    chat_messages = service.get_chat_messages_by_tabId(tabId)
+
+    response_data = ChatMessagesResponse(
+        id=chat_tab.id,
+        name=chat_tab.name,
+        created_at=chat_tab.created_at,
+        updated_at=chat_tab.updated_at,
+        messages=chat_messages,
+    )
+
+    return ResponseMessage.success(value=response_data, code=CommonCode.SUCCESS_GET_CHAT_MESSAGES)
